a8d5273ee2ea76248df1a31ae6c5943c8d77a3f6
shekhar249 <bhagyawantimobile@gmail.com>
2026-02-26 09:02:11 +0530

    Add 'bhagyesh' to possible names array

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ interface PassengerForm {
 
 const HomePage = () => {
   const router = useRouter()
-  const possible = ["shekar", "Shoaib","Mohammad Suhaib Amir","omkar","nitin","Nitin","Shubham","shubham","omkar"]
+  const possible = ["shekar", "Shoaib","Mohammad Suhaib Amir","omkar","nitin","Nitin","Shubham","shubham","omkar","bhagyesh","Bhagyesh"]
   const [passengerForm, savePassengerForm] =
     useLocalStorageOnce<PassengerForm | null>("passengerForm", null);
   // ---------------- Popup State ----------------
