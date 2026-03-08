43bc90959dae21ed134b45b469368fbfd369c490
shekhar249 <bhagyawantimobile@gmail.com>
2026-03-08 08:51:04 +0530

    Add 'Amith' to possible passenger names

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ interface PassengerForm {
 
 const HomePage = () => {
   const router = useRouter()
-  const possible = ["shekar", "Shoaib","Mohammad Suhaib Amir","omkar","nitin","Nitin","Shubham","shubham","omkar","bhagyesh","Bhagyesh"]
+  const possible = ["shekar", "Shoaib","Mohammad Suhaib Amir","omkar","nitin","Nitin","Shubham","shubham","omkar","bhagyesh","Bhagyesh","Amith","amith"]
   const [passengerForm, savePassengerForm] =
     useLocalStorageOnce<PassengerForm | null>("passengerForm", null);
   // ---------------- Popup State ----------------
